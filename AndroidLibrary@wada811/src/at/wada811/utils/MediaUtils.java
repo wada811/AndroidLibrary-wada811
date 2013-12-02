53b063e9523db1616f0d0126085cbbbe6d021cd2
wada811 <at.wada811@gmail.com>
2013-12-02 19:22:50 +0900

    change the way to get external storage path

--- AndroidLibrary@wada811/src/at/wada811/utils/MediaUtils.java
+++ AndroidLibrary@wada811/src/at/wada811/utils/MediaUtils.java
@@ -15,15 +15,6 @@
  */
 package at.wada811.utils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
@@ -36,149 +27,15 @@ import android.provider.MediaStore.Images.ImageColumns;
 import android.provider.MediaStore.MediaColumns;
 import android.webkit.MimeTypeMap;
 import at.wada811.constant.MediaConstant;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MediaUtils {
 
-    /**
-     * 保存するディレクトリのパスを取得する
-     * 
-     * @param context アプリ内部領域を除外したい場合は null を渡す
-     * @return save dir path, or null
-     */
-    public static String getSaveDirPath(Context context){
-        String saveDirPath = MediaUtils.getExternalSdCardPath();
-        LogUtils.v(saveDirPath);
-        if(saveDirPath == null){
-            saveDirPath = MediaUtils.getInternalSdCardParh();
-            LogUtils.v(saveDirPath);
-        }
-        if(saveDirPath == null && context != null){
-            saveDirPath = context.getFilesDir().getAbsolutePath();
-            LogUtils.v(saveDirPath);
-        }
-        return saveDirPath;
-    }
-
-    /**
-     * 外部SDカードのパスを取得する
-     * 
-     * @return external SD card path, or null
-     */
-    public static String getExternalSdCardPath(){
-        HashSet<String> paths = new HashSet<String>();
-        Scanner scanner = null;
-        try{
-            // システム設定ファイルを読み込み
-            File vold_fstab = new File("/system/etc/vold.fstab");
-            scanner = new Scanner(new FileInputStream(vold_fstab));
-            while(scanner.hasNextLine()){
-                String line = scanner.nextLine();
-                LogUtils.e(line);
-                // dev_mountまたはfuse_mountで始まる行
-                if(line.startsWith("dev_mount") || line.startsWith("fuse_mount")){
-                    // 半角スペースではなくタブで区切られている機種もあるらしい
-                    // 半角スペース区切り３つめ（path）を取得
-                    String path = line.replaceAll("\t", " ").split(" ")[2];
-                    paths.add(path);
-                }
-            }
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
-            LogUtils.e(e);
-            return null;
-        }finally{
-            if(scanner != null){
-                scanner.close();
-            }
-        }
-        // Environment.getExternalStorageDirectory() が内部SDを返す場合は除外
-        if(!Environment.isExternalStorageRemovable()){
-            paths.remove(Environment.getExternalStorageDirectory().getPath());
-        }
-        // マウントされているSDカードのパスを追加 
-        List<String> mountSdCardPaths = new ArrayList<String>();
-        for(String path : paths){
-            if(isMounted(path)){
-                LogUtils.v(path);
-                mountSdCardPaths.add(path);
-            }
-        }
-        // マウントされているSDカードのパス
-        String mountSdCardPath = null;
-        if(mountSdCardPaths.size() > 0){
-            mountSdCardPath = mountSdCardPaths.get(0);
-        }
-        return mountSdCardPath;
-
-    }
-
-    /**
-     * パスがマウントされているSDカードのパスかチェックする
-     * 
-     * @param path SD card path
-     * @return true if path is the mounted SD card's path, otherwise false
-     */
-    public static boolean isMounted(String path){
-        boolean isMounted = false;
-        Scanner scanner = null;
-        try{
-            // マウントポイントを取得する
-            File mounts = new File("/proc/mounts");
-            scanner = new Scanner(new FileInputStream(mounts));
-            while(scanner.hasNextLine()){
-                if(scanner.nextLine().contains(path)){
-                    isMounted = true;
-                    break;
-                }
-            }
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
-            LogUtils.e(e);
-            return false;
-        }finally{
-            if(scanner != null){
-                scanner.close();
-            }
-        }
-        return isMounted;
-    }
-
-    /**
-     * 内部SDカードのパスを取得する
-     * 
-     * @return internal SD card path, or null
-     */
-    public static String getInternalSdCardParh(){
-        if(Environment.isExternalStorageRemovable()){
-            return null;
-        }else if(MediaUtils.checkSdCardMounted()){
-            return Environment.getExternalStorageDirectory().getAbsolutePath();
-        }else{
-            return null;
-        }
-    }
-
-    /**
-     * SDカードにアプリ用ディレクトリを作成する
-     * 
-     * @param context
-     * @return
-     */
-    public static File getExternalStorageAppDir(Context context, String dirName){
-        File file = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), dirName);
-        if(!file.exists()){
-            file.mkdirs();
-        }
-        return file;
-    }
-
-    /**
-     * SDカードのパスを取得する
-     * 
-     * @return path of SD Card
-     */
-    public static String getExternalStoragePath(){
-        return Environment.getExternalStorageDirectory().getPath();
+    public static File getExternalStorageDirectory(String dirName){
+        return new File(Environment.getExternalStorageDirectory(), dirName);
     }
 
     /**
@@ -186,24 +43,18 @@ public class MediaUtils {
      * 
      * @return マウントしていれば true
      */
-    public static boolean checkSdCardMounted(){
-        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+    public static boolean isSdCardMounted(){
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
     }
 
     /**
-     * SDカードに十分な空き容量があるか調べる
+     * 十分な空き容量があるか調べる
      * 
-     * @return SDカードに十分な空き容量があれば true
+     * @return 十分な空き容量があれば true
      */
-    public static boolean checkAvailableSpaceInSdCard(double limitMinimumSpace){
-        String externalStoragePath = MediaUtils.getExternalStoragePath();
-        File file = new File(externalStoragePath);
+    public static boolean isAvailableSpace(File file, double limitMinimumSpace){
         double availableSpaceInSdCard = file.getUsableSpace() / 1024; // (MB)
-        if(availableSpaceInSdCard >= limitMinimumSpace){
-            return true;
-        }else{
-            return false;
-        }
+        return availableSpaceInSdCard >= limitMinimumSpace;
     }
 
     public static boolean saveBitmap(Context context, Bitmap bitmap, String filePath, int orientation){
@@ -317,5 +168,4 @@ public class MediaUtils {
         }
         return mimeType;
     }
-
 }
